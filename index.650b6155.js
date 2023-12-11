ff7d5a3443515cae6ba88461ec7a77382a60cd8e
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-11 20:04:44 +0100

    make build

---
+++ index.650b6155.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let t in n){let l=document.createElement("li");l.textContent=t,"object"==typeof n[t]&&Object.keys(n[t]).length>0&&e(l,n[t]),r.appendChild(l)}return t.appendChild(r),r}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.650b6155.js.map
